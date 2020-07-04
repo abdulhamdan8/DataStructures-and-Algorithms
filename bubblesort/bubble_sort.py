d4f7b1102c4d2d5a565914426bd5342638c0bf44
Abdul Kadar Hamdan <Abdul Kadar Hamdan@DESKTOP-PJ51CAF>
2020-07-04 13:04:34 +0530

    bubble sort added

---
+++ bubblesort/bubble_sort.py
@@ -0,0 +1,14 @@
+# day 1
+# bubble sort
+def bubble_sort(a):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+    return a
+
+# driver code
+arr=[9,8,76,5,4]
+print(bubble_sort(arr))
+
+
