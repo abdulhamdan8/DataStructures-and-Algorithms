53da758993ccfab995b59735970ec021527d0c5f
Abdul Kadar Hamdan <Abdul Kadar Hamdan@DESKTOP-PJ51CAF>
2020-07-06 10:16:55 +0530

    merge sort added

---
+++ insertion Sort/insertion_sort.py
@@ -0,0 +1,19 @@
+# day 3
+# merge Sort
+# time complexity O(n2)
+
+
+def merge_sort(ar):
+    for i in range(1, len(ar)):
+        current = ar[i]
+        j = i - 1
+        while j >= 0 and ar[j] > current:
+            ar[j + 1] = ar[j]
+            j -= 1
+        ar[j + 1] = current
+    return ar
+
+
+# driver code
+arr = list(map(int, input().split()))
+print(merge_sort(arr))
